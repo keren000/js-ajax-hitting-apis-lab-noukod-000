4b7234eba7a7b8d2db7a899814ff6d0a596e06c4
Keren-Happuc Fleurimond <keren@noukod.com>
2019-03-11 14:40:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 function getRepositories() {
     let username = document.getElementById("username").value;
 
-    const req = new XMLHttpRequest();
-    req.addEventListener('load', displayRepositories);
-    req.open('GET', `https://api.github.com/users/${username}/repos`);
-    req.send();
+    const request = new XMLHttpRequest();
+    request.addEventListener('load', displayRepositories);
+    request.open('GET', `https://api.github.com/users/${username}/repos`);
+    requestreq.send();
 }
 
 function displayRepositories() {
